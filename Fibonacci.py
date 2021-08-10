ac817bc478eb4702ae5e21680f00d056c021aba3
__Error404__ <68526850+AntonioUribe@users.noreply.github.com>
2021-08-10 17:28:09 -0500

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,24 @@
+# Portafolio
+# Github 
+from matplotlib import pyplot as plt
+
+class Fibonacci():
+    def __init__(self,iniciales,posicion):
+        self.iniciales=iniciales
+        self.posicion=posicion
+
+    def agregar(self):
+        for i in range(self.posicion-2):
+            r = self.iniciales[i]+ self.iniciales[i+1]
+            self.iniciales.append(r)
+        r=0
+        print(self.iniciales)
+        return self.iniciales
+    def grafica(self):
+        return self.agregar()
+
+a=Fibonacci([0,1],10).grafica()
+fig, ax = plt.subplots()
+ax.scatter(x = [x for  x in range(len(a))], y =a)
+plt.savefig('diagrama-dispersion.png')
+plt.show()
