9a6a56585451ec03808af1faf40c201eea6d9200
__Error404__ <68526850+AntonioUribe@users.noreply.github.com>
2021-09-16 01:08:21 -0500

    Update numerosRomanos.py

--- numerosRomanos.py
+++ numerosRomanos.py
@@ -1,23 +1,4 @@
-#numero del 1-10
-'''
-num=[10, 9, 5, 4,1]
-romano = ["X", "IX", "V", "IV","I"]
-resultado = ''
-numero = int(input("Escribe numero: "))
-i=0
-while i<len(num):
-    
-    
-    if numero // num[i] !=0:
-        resultado += romano[i]
-        numero-=num[i]
-    else:
-        i+=1      
-    
-        
-print(resultado)
-'''
-
+#Numero romanos de 1-40
 class Num_Romano:
     
     def __init__(self,numero,num,romano):
@@ -41,30 +22,3 @@ class Num_Romano:
             El numero Romano es:  {resultado}
                 '''                   
 print(Num_Romano(4,[40,10, 9, 5, 4,1],["XL","X", "IX", "V", "IV","I"]))
-
-
-'''
-class Num_Romano:
-    
-    def __init__(self,numero):
-        self.numero = numero
-        
-        
-    
-    def __str__(self):
-
-        i=0
-        num=[10, 9, 5, 4,1]
-        romano = ["X", "IX", "V", "IV","I"]
-        resultado = ""
-        while i<len(num):
-            if self.numero // num[i] !=0:
-                resultado += romano[i]
-                self.numero-=num[i]
-            else:
-                i+=1
-        return resultado
-
-print(Num_Romano(9))
-'''
-    
